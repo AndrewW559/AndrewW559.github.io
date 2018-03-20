f036a4ee0e1f009ca25635f46f50b41cc596ba40
AndrewW559 <35267868+AndrewW559@users.noreply.github.com>
2018-03-20 08:38:40 -0600

    Create script6.js

---
+++ script6.js
@@ -0,0 +1,28 @@
+  function openAccordion1(){
+$( function() {
+    $( "#accordion" ).accordion({
+      active: false,
+      collapsible: true
+      
+    });
+  } );
+  }
+  
+  function openAccordion2(){
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      active: false
+    });
+  } );
+  }
+
+  function openAccordion3(){
+$( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true,
+      active:false
+      
+    });
+  } );
+  }  
